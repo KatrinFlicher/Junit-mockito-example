f86ba198711fe5b540477b9a6bcf68024279e373
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-01-08 22:50:28 +0300

    Add to TodoBusinessImplMockTest.java and ListTest.java additional tests with using BDD and Mockito

--- src/test/java/by/training/katrinflicher/business/TodoBusinessImplMockTest.java
+++ src/test/java/by/training/katrinflicher/business/TodoBusinessImplMockTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,5 +26,15 @@ public class TodoBusinessImplMockTest {
         List<String> filteredTodos = todoBusiness.retrieveTodosRelatedToSpring("Dummy");
         assertEquals(2, filteredTodos.size());
         assertTrue(filteredTodos.get(0).contains("Spring")&&filteredTodos.get(1).contains("Spring"));
+    }
+
+    @Test
+    public void testUsingMockito_UsingBDD(){
+        TodoService todoService = mock(TodoService.class);
+        TodoBusinessImpl todoBusiness = new TodoBusinessImpl(todoService);
+        List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+        given(todoService.retrieveTodos("Dummy")).willReturn(allTodos);
+        List<String> filteredTodos = todoBusiness.retrieveTodosRelatedToSpring("Dummy");
+        assertThat(filteredTodos.size(), is(2));
     }
 }
---
+++ src/test/java/by/training/katrinflicher/mockito/ListTest.java
@@ -0,0 +1,53 @@
+package by.training.katrinflicher.mockito;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+public class ListTest {
+
+    @Test
+    public void letsMockListSize(){
+        List list = mock(List.class);
+        when(list.size()).thenReturn(2);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void letsMockListSizeWithMultipleReturnValues(){
+        List list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20);
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    public void letsMockListGet(){
+        List list = mock(List.class);
+        when(list.get(0)).thenReturn("KatrinFlicher");
+        assertEquals("KatrinFlicher", list.get(0));
+        assertEquals(null, list.get(1));
+    }
+
+    @Test
+    public void letsMockListGetWithAny(){
+        List list = mock(List.class);
+        when(list.get(anyInt())).thenReturn("KatrinFlicher");
+        assertEquals("KatrinFlicher",list.get(0));
+        assertEquals("KatrinFlicher",list.get(1));
+    }
+
+    @Test
+    public void bddAliases_UsingGivenWillReturn(){
+        List list = mock(List.class);
+        given(list.get(anyInt())).willReturn("Katrin");
+        assertThat(list.get(0), is("Katrin"));
+        assertThat(list.get(1), is("Katrin"));
+
+    }
+}
