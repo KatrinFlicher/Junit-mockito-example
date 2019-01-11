9ac348b6aa4b85b30159f6baa9ccf13605ee1146
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-01-11 13:14:59 +0300

    Using PowerMock and Mockito to mock a Static Method.

---
+++ src/main/java/by/training/katrinflicher/powermock/UtilityClass.java
@@ -0,0 +1,10 @@
+package by.training.katrinflicher.powermock;
+
+public class UtilityClass {
+    static int staticMethod(long value) {
+        // Some complex logic is done here...
+        throw new RuntimeException(
+                "I don't want to be executed. I will anyway be mocked out.");
+    }
+
+}
